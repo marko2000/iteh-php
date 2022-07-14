e61db2a2cf0b583a8f8bde000fd1f732eddfcead
marko2000 <mmarkom2000@gmail.com>
2022-07-14 19:50:22 +0200

    Fixed ocene.php, telefoni.php and typos

--- ocene.php
+++ ocene.php
@@ -1,16 +1,15 @@
 <?php
-include 'models/Ocena.php';
+    include 'models/Ocena.php';
 
-if (isset($_POST['ocena_kriterijum'])) {
+    if (isset($_POST['ocena_kriterijum'])) {
+        $ocena = new Ocena();
+        $ocena->setOcena_kriterijum($_POST['ocena_kriterijum']);
+        $ocena->setTelefon_ocena($_POST['telefon_ocena']);
+        $ocena->setTelefon_opis($_POST['telefon_opis']);
+        $ocena->setTelefon_id($_POST['telefon_id']);
 
-    $ocena = new Ocena();
-    $ocena->setOcena_kriterijum($_POST['ocena_kriterijum']);
-    $ocena->setTelefon_ocena($_POST['telefon_ocena']);
-    $ocena->setTelefon_opis($_POST['telefon_opis']);
-    $ocena->setTelefon_id($_POST['telefon_id']);
-
-    $ocena->save();
-}
+        $ocena->save();
+    }
 ?>
 
 <!DOCTYPE HTML>
@@ -27,58 +26,56 @@ if (isset($_POST['ocena_kriterijum'])) {
     <script src="js/jquery.min.js"></script>
     <script src="js/script.js"></script>
 
-
     <script src="//cdn.ckeditor.com/4.5.5/basic/ckeditor.js"></script>
 
     <script>
         $.get("controller.php", {
-                ocena: "show"
-            })
-            .done(function(data) {
-                var result = '';
-                var finalData = JSON.parse(data);
-                result = '';
-                for (var i = 0; i < finalData.length; i++) {
-                    result += '<div class="blog_grid">' +
-                        '<p style="font-size: 1.1rem;">' + finalData[i].ocena_kriterijum +  '</p>' +
-                        '<p class="telefon-opis"">' + finalData[i].telefon_opis + '</p>' +
-                        '<ul class="links">' +
-                        '<li><button type="button" onclick="izbrisi(' + finalData[i].ocena_id + ')" >Obrisi</button></li>' +
-                        '</ul>' +
-                        '</div>';
-                };
-                $('#sveOcene').html(result);
-            });
+            ocena: "show"
+        }).done(function(data) {
+            var result = '';
+            var finalData = JSON.parse(data);
+            result = '';
+            for (var i = 0; i < finalData.length; i++) {
+                result += '<div class="blog_grid">' +
+                '<p style="font-size: 1.1rem;">' + finalData[i].ocena_kriterijum +  '</p>' +
+                '<p class="telefon-opis"">' + finalData[i].telefon_opis + '</p>' +
+                '<ul class="links">' +
+                '<li><button type="button" onclick="izbrisi(' + finalData[i].ocena_id + ')" >Obrisi</button></li>' +
+                '</ul>' +
+                '</div>';
+            };
 
-        // Spisak telefona
+            $('#sveOcene').html(result);
+        });
+
         $.get("controller.php", {
-                telefon: "show"
-            })
-            .done(function(data) {
-                var result = '';
-                var finalData = JSON.parse(data);
-                for (var i = 0; i < finalData.length; i++) {
-                    result += '<option  value=' + finalData[i].telefon_id + '>' + finalData[i].telefon_naziv + '</option>';
-                };
-                $('#spisakTelefona').html(result);
-            });
+            telefon: "show"
+        }).done(function(data) {
+            var result = '';
+            var finalData = JSON.parse(data);
+            for (var i = 0; i < finalData.length; i++) {
+                result += '<option  value=' + finalData[i].telefon_id + '>' + finalData[i].telefon_naziv + '</option>';
+            };
 
+            $('#spisakTelefona').html(result);
+        });
 
         function izbrisi(id) {
             $.get('controller.php', {
-                    izbrisi: 'ocena',
-                    id: id
-                })
-                .done(function(data) {
-                    if (data == 'OK') {
-                        location.reload();
-                    } else {
-                        alert('Greska');
-                    }
-                });
+                izbrisi: 'ocena',
+                id: id
+            }).done(function(data) {
+                if (data == 'OK') {
+                    location.reload();
+                } else {
+                    alert('Greska');
+                }
+            });
         }
     </script>
+</head>
 
+<body>
     <nav class="navbar navbar-custom">
         <ul class="nav navbar-nav navbar-custom">
             <a href="index.php" class="navbar-left"><img src="images/logo.png" class="logo"></a>
@@ -91,9 +88,8 @@ if (isset($_POST['ocena_kriterijum'])) {
     <div class="about">
         <div class="container">
             <section class="title-section">
-                <h1 class="text-center" class="title-header"> Informacije o ocenama </h1>
-                <a href="index.php">
-                    < Povratak</a>
+                <h1 class="text-center" class="title-header">Informacije o ocenama</h1>
+                <a href="index.php">< Povratak</a>
             </section>
         </div>
     </div>
@@ -103,8 +99,6 @@ if (isset($_POST['ocena_kriterijum'])) {
             <h1 class="text-center">Unos ocene</h1>
 
             <div class="box">
-
-                <p>
                 <form class="form-group" action="" method="POST" name="unos">
                     <p>Kriterijum</p>
                     <input class="form-control form-input" type="text" name="ocena_kriterijum">
@@ -120,16 +114,13 @@ if (isset($_POST['ocena_kriterijum'])) {
                     <input type="text" class="form-control form-input" name="telefon_ocena">
                     <p></p><br><br>
                     <button type="submit" class="form-control" class="btn btn-danger">Oceni</button>
-
                 </form>
-                </p>
             </div>
 
             <div class="row sve-ocene">
-            <span>Sve ocene:</span>
-                <div class="col-md-6" id="sveOcene">
-                    
-                </div>
+                <span>Sve ocene:</span>
+                <div class="col-md-6" id="sveOcene"></div>
             </div>
         </div>
     </div>
+</body>
--- telefoni.php
+++ telefoni.php
@@ -1,11 +1,11 @@
 <?php
-include 'models/Telefon.php';
+    include 'models/Telefon.php';
 
-if (isset($_POST['telefon_naziv'])) {
-    $telefon = new Telefon();
-    $telefon->setTelefon_naziv($_POST['telefon_naziv']);
-    $telefon->save();
-}
+    if (isset($_POST['telefon_naziv'])) {
+        $telefon = new Telefon();
+        $telefon->setTelefon_naziv($_POST['telefon_naziv']);
+        $telefon->save();
+    }
 ?>
 
 <!DOCTYPE HTML>
@@ -22,44 +22,43 @@ if (isset($_POST['telefon_naziv'])) {
     <script src="js/jquery.min.js"></script>
     <script src="js/script.js"></script>
 
-
     <script src="//cdn.ckeditor.com/4.5.5/basic/ckeditor.js"></script>
 
     <script>
         $.get("controller.php", {
-                telefon: "show"
-            })
-            .done(function(data) {
-                var result = '';
-                var finalData = JSON.parse(data);
-                result = '';
-                for (var i = 0; i < finalData.length; i++) {
-                    result += '<div class="blog_grid">' +
-                        '<p>' + finalData[i].telefon_naziv + '</p>' +
-                        '<ul class="links">' +
-                        '<li><button type="button" onclick="izbrisi(' + finalData[i].telefon_id + ')" >Obrisi</button></li>' +
-                        '</ul>' +
-                        '</div>';
-                };
-                $('#sviTelefoni').html(result);
-            });
+            telefon: "show"
+        }).done(function(data) {
+            var result = '';
+            var finalData = JSON.parse(data);
+            result = '';
+            for (var i = 0; i < finalData.length; i++) {
+                result += '<div class="blog_grid">' +
+                '<p>' + finalData[i].telefon_naziv + '</p>' +
+                '<ul class="links">' +
+                '<li><button type="button" onclick="izbrisi(' + finalData[i].telefon_id + ')" >Obrisi</button></li>' +
+                '</ul>' +
+                '</div>';
+            };
 
+            $('#sviTelefoni').html(result);
+        });
 
         function izbrisi(id) {
             $.get('controller.php', {
-                    izbrisi: 'telefon',
-                    id: id
-                })
-                .done(function(data) {
-                    if (data == 'OK') {
-                        location.reload();
-                    } else {
-                        alert('Greska');
-                    }
-                });
+                izbrisi: 'telefon',
+                id: id
+            }).done(function(data) {
+                if (data == 'OK') {
+                    location.reload();
+                } else {
+                    alert('Greska');
+                }
+            });
         }
     </script>
+</head>
 
+<body>
     <nav class="navbar navbar-custom">
         <ul class="nav navbar-nav navbar-custom">
             <a href="index.php" class="navbar-left"><img src="images/logo.png" class="logo"></a>
@@ -72,9 +71,8 @@ if (isset($_POST['telefon_naziv'])) {
     <div class="about">
         <div class="container">
             <section class="title-section">
-                <h1 class="text-center" class="title-header"> Informacije o telefonima </h1>
-                <a href="index.php">
-                    < Povratak</a>
+                <h1 class="text-center" class="title-header">Informacije o telefonima</h1>
+                <a href="index.php">< Povratak</a>
             </section>
         </div>
     </div>
@@ -82,34 +80,17 @@ if (isset($_POST['telefon_naziv'])) {
     <div class="container">
         <div id="content_left">
             <h1 class="text-center">Unos telefona</h1>
-
             <div class="box">
-
-                <p>
-
-
-
-
-
-
-
                 <form class="form-group" action="" method="POST" name="unos">
                     <p>Naziv telefona</p>
-                    <input class="form-control" type="text" name="telefon_naziv">
-                    <p></p><br><br>
+                    <input class="form-control" type="text" name="telefon_naziv"><br><br>
                     <button type="submit" class="form-control" class="btn btn-danger">Dodaj</button>
                 </form>
-
-
-
-
-
-                </p>
             </div>
             <div class="row sve-ocene">
                 <span>Svi telefoni:</span>
-                <div class="col-md-8" id="sviTelefoni">
-                </div>
+                <div class="col-md-8" id="sviTelefoni"></div>
             </div>
         </div>
     </div>
+</body>
